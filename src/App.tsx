ece0f81c434e5a0650f31f3e86045f004da40843
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2023-12-17 16:28:01 +0700

    add fetch data from api and create post cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import NewsCard from "./components/NewsCard/NewsCard";
+
+interface ApiResponse {
+    response: {
+        results: IPost[];
+    };
+}
+
+interface IPost {
+    id: string;
+    type: string;
+    webTitle: string;
+    webUrl: string;
+    fields: IFields;
+    webPublicationDate: string;
+}
+
+interface IFields {
+    thumbnail: string;
+    trailtext: string;
+    body: string;
+    publication: string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [news, setNews] = useState<IPost[]>([]);
+
+    async function getPosts(): Promise<IPost[]> {
+        const { data } = await axios.get<ApiResponse>(
+            "https://content.guardianapis.com/search?api-key=24666356-cbba-47ca-ae0d-c98a27166f0f&show-fields=trailText,body,thumbnail,publication&show-elements=image"
+        );
+
+        return data.response.results;
+    }
+
+    async function fetchData() {
+        try {
+            const posts = await getPosts();
+            setNews(posts);
+            console.log(posts);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className='App'>
+            <div className='container'>
+                {news.map((i) => (
+                    <NewsCard
+                        key={i.id}
+                        image={i.fields.thumbnail}
+                        webTitle={i.webTitle}
+                        webPublicationdate={i.webPublicationDate}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NewsCard/NewsCard.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import cl from "./NewsCard.module.css";
+import moment from "moment";
+
+interface ICardProps {
+    image: string;
+    webTitle: string;
+    webPublicationdate: string;
+}
+
+const NewsCard: FC<ICardProps> = ({ image, webTitle, webPublicationdate }) => {
+    return (
+        <div className={cl.card}>
+            <img src={image} alt='#' className={cl.card_img} />
+            <div className={cl.post_date}>
+                {moment(webPublicationdate).format("DD MMMM YYYY HH:mm")}{" "}
+            </div>
+            <div className={cl.card_text}>{webTitle}</div>
+            <button className={cl.card_button}>Details</button>
+        </div>
+    );
+};
+
+export default NewsCard;
